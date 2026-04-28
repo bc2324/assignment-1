1fa79f7dc1aa4b3dbd36e6c819de171fccd30bef
bc2324 <bc2324@uw.edu>
2026-04-28 15:56:14 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <h1>Notes</h1>
+        <button className="add-btn">+ Add note</button>
+      </header>
+
+      <main className="main"></main>
+    </div>
+  )
+}
+
+export default App
